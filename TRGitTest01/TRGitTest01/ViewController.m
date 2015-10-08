501267ff8de8d2ed78d550746a3822c871fefe30
apple <apple@apples-MacBook-Pro.local>
2015-10-08 12:02:30 +0800

    123

---
+++ TRGitTest01/TRGitTest01/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TRGitTest01
+//
+//  Created by apple on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"第一个文件");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
